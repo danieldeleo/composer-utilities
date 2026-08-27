ddb030f5de377a4c4b2e20e6e4179e69befa406a
ddeleo <ddeleo@google.com>
2026-08-27 09:18:58 -0400

    refactor: update BigQuery load test DAGs with chained bash task mapping and modify agy command flags

--- cicd/dags/bq_1000_queries.py
+++ cicd/dags/bq_1000_queries.py
@@ -2,21 +2,28 @@ import datetime
 
 from airflow import DAG
 from airflow.decorators import task
+from airflow.operators.bash import BashOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 
 
 @task
-def generate_query_configs():
-    # Generate 1000 configurations for the BigQueryInsertJobOperator
-    return [
-        {
-            "query": {
-                "query": "SELECT 1",
-                "useLegacySql": False,
-            }
+def generate_bash_commands():
+    return [f"echo {i}" for i in range(1000)]
+
+
+@task
+def make_bq_config(number: str):
+    return {
+        "query": {
+            "query": f"SELECT {number}",
+            "useLegacySql": False,
         }
-        for _ in range(1000)
-    ]
+    }
+
+
+@task
+def generate_print_commands(job_id: str):
+    return f"echo {job_id}"
 
 
 with DAG(
@@ -26,9 +33,20 @@ with DAG(
     catchup=False,
     tags=["bigquery", "load_test"],
 ) as dag:
-    query_configs = generate_query_configs()
+    bash_commands = generate_bash_commands()
 
-    # Use dynamic task mapping to expand the operator into 1000 tasks
+    emit_number = BashOperator.partial(task_id="emit_number", do_xcom_push=True).expand(
+        bash_command=bash_commands
+    )
+
+    bq_configs = make_bq_config.expand(number=emit_number.output)
+
     bq_tasks = BigQueryInsertJobOperator.partial(
-        task_id="run_select_1",
-    ).expand(configuration=query_configs)
+        task_id="run_select",
+    ).expand(configuration=bq_configs)
+
+    print_commands = generate_print_commands.expand(job_id=bq_tasks.output)
+
+    print_results = BashOperator.partial(
+        task_id="print_result",
+    ).expand(bash_command=print_commands)
