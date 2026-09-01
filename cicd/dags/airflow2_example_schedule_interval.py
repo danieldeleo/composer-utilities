3201ba005cafacc4c38b98ac0cc01504b85f43bc
Cloud Build <cloudbuild@danny-bq.iam.gserviceaccount.com>
2026-09-01 17:49:22 +0000

    Automated updates from Antigravity CLI

--- cicd/dags/airflow2_example_schedule_interval.py
+++ cicd/dags/airflow2_example_schedule_interval.py
@@ -5,30 +5,31 @@
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.utils.dates import days_ago
+import datetime
 
-# Airflow 3 Breaking Changes demonstrated here:
-# 1. schedule_interval argument is removed in Airflow 3. Use schedule instead.
-# 2. airflow.utils.dates.days_ago is removed in Airflow 3.
-# 3. DummyOperator from airflow.operators.dummy_operator is removed in Airflow 3.
+import pendulum
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
 with DAG(
     dag_id="airflow2_example_schedule_interval",
-    schedule_interval="@daily",
-    start_date=days_ago(2),
+    schedule="@daily",
+    start_date=pendulum.datetime(2026, 1, 1, tz="UTC"),
     catchup=False,
+    default_args={
+        "retries": 2,
+        "retry_delay": datetime.timedelta(minutes=5),
+    },
     tags=["airflow2", "compatibility_test"],
 ) as dag:
-    start = DummyOperator(task_id="start_task")
+    start = EmptyOperator(task_id="start_task")
 
-    end = DummyOperator(task_id="end_task")
+    end = EmptyOperator(task_id="end_task")
 
     start >> end
